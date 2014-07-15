35dfb0fcf236b5bfd17d83894fd275a480896535
Yuri Chechulin <chechulin.yuri@gmailc.com>
2014-07-15 15:07:02 +0700

    delete unused class

--- src/com/noveogroup/device/Phone.java
+++ src/com/noveogroup/device/Phone.java
@@ -3,7 +3,7 @@ package com.noveogroup.device;
 /**
  * Created by Admin on 15.07.14.
  */
-public class Phone extends DeviceClass {
+public class Phone extends AbstractDevice {
 
     public Phone(int pr) {
         super(pr);
