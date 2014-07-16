f494faada173eccc55bed18b5b8ca7692f51f4cf
Yuri Chechulin <chechulin.yuri@gmailc.com>
2014-07-16 12:01:43 +0700

    update main file

--- src/com/noveogroup/main/Main.java
+++ src/com/noveogroup/main/Main.java
@@ -5,19 +5,41 @@ import com.noveogroup.algorithm.*;
 
 public class Main {
 
+    static Device[] devices;
+
     public static void main(String[] args) {
         //You can check your algorithms here.
 
-        Device[] devices = new Device[3];
+        devices = new Device[5];
         devices[0] = new Phone(25);
         devices[1] = new Tablet(15);
         devices[2] = new Notebook(50);
+        devices[3] = new Notebook(120);
+        devices[4] = new Phone(10);
 
-        System.out.println(devices[0].getPrice() + " " + devices[1].getPrice() + " " + devices[2].getPrice());
+        printDevices();
 
-        QuickSort bs = new QuickSort();
+        BubbleSort bs = new BubbleSort();
         bs.sort(devices, true);
 
-        System.out.println(devices[0].getPrice() + " " + devices[1].getPrice() + " " + devices[2].getPrice());
+        printDevices();
+
+        InsertionSort is = new InsertionSort();
+        is.sort(devices, false);
+
+        printDevices();
+
+        QuickSort qs = new QuickSort();
+        qs.sort(devices, true);
+
+        printDevices();
+    }
+
+    public static void printDevices() {
+        String output = new String();
+        for (int i = 0; i < devices.length; i++) {
+            output = output.concat(devices[i].getPrice() + " ");
+        }
+        System.out.println(output);
     }
 }
